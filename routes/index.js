20ede2b4596a9ba161b7822e4180eb485e397359
Dino Horvat <dxh3401@rit.edu>
2019-01-22 23:54:25 +0100

    Connect wifi

--- routes/index.js
+++ routes/index.js
@@ -49,32 +49,26 @@ router.get('/wifi', (req, res) => {
 
 /* SETUP wifi auto script. */
 router.get('/wifiNetworks', (req, res) => {
-
   wifi.scan(function(err, networks) {
     if (err) {
       console.log(err);
     } else {
       console.log(networks);
       res.send({data: networks, success: true});
-      /*
-      networks = [
-          {
-            ssid: '...',
-            bssid: '...',
-            mac: '...', // equals to bssid (for retrocompatibility)
-            channel: <number>,
-            frequency: <number>, // in MHz
-            signal_level: <number>, // in dB
-            quality: <number>, // same as signal level but in %
-            security: 'WPA WPA2' // format depending on locale for open networks in Windows
-            security_flags: '...' // encryption protocols (format currently depending of the OS)
-            mode: '...' // network mode like Infra (format currently depending of the OS)
-          },
-          ...
-      ];
-      */
     }
   });
 });
 
+router.post('/createAlbum/:name',  (req, res) => {
+  let wifiInfo = req.body;
+
+// Connect to a network
+  wifi.connect({ ssid : wifiInfo.ssid, password : wifiInfo.password}, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Connected');
+    res.send({success: true})
+  });
+});
 module.exports = router;
